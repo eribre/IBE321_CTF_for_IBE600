8e22737ad98b104bb67811d28c7b839e9b7a8076
eribre <70907050+eribre@users.noreply.github.com>
2023-04-11 23:38:20 +0000

    black formatter

--- main.py
+++ main.py
@@ -27,6 +27,7 @@ from sql_injection import *
 
 port = 81
 host = "0.0.0.0"
+debug = True
 
 if __name__ == "__main__":
-    app.run(host=host, port=port)
+    app.run(host=host, port=port, debug=debug)
