9b87f898cb5a9f438118e9d9b1bcbc543c3f7729
Erik Brendehaug <erik.brendehaug@himolde.no>
2023-04-23 01:24:19 +0200

    Improve traversal

--- traversal.py
+++ traversal.py
@@ -16,14 +16,20 @@ def traversal():
     return render_template("traversal/traversal.j2")
 
 
+# * Note, this code is not the best way to do this, but it works on any OS.
+# Path Traversal Confirm
 @app.route("/traversal/confirm", methods=["get"])
 def traversal_confirm():
     inp_path = request.args["file"]
 
     count_dots = inp_path.count("../")
     count_path = inp_path.count("/etc/shadow")
+    check_last = inp_path.split("/")[-2:] == ["etc", "shadow"]
+    check_random = not any(
+        [x for x in inp_path.split("/") if x not in ["..", "etc", "shadow"]]
+    )
 
-    if count_dots >= 3 and count_path == 1:
+    if count_dots >= 3 and count_path == 1 and check_last and check_random:
         return render_template("traversal/directory.j2", usrInp=inp_path)
     else:
         return render_template("traversal/dir_fail.j2", usrInp=inp_path)
