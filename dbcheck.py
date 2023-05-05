52d666cc9b75aeba3391e2fa822503dce48741e1
Erik Brendehaug <erik.brendehaug@himolde.no>
2023-05-05 04:32:54 +0200

    db tools comments

--- dbcheck.py
+++ dbcheck.py
@@ -1,7 +1,9 @@
 from dbconnection import Database
 
+# A simple script to check if the database is working correctly
+
 with Database() as db:
-	result = db("SELECT * FROM users")
-	result = result.fetchall()
-	for i in result:
-		print(i)
+    result = db("SELECT * FROM users")
+    result = result.fetchall()
+    for i in result:
+        print(i)
--- dbreset.py
+++ dbreset.py
@@ -2,18 +2,24 @@ import sqlite3
 import os
 from dbconnection import Database
 
-path = './user_db.db'
+# A script to reset the database to its original state
+
+path = "./user_db.db"
 
 if os.path.isfile(path):
-	os.remove("user_db.db")
-	
+    os.remove("user_db.db")
+
 with Database() as db:
-	create = db("""CREATE TABLE users(id INTEGER PRIMARY KEY, 
- 		name TEXT NOT NULL, password TEXT NOT NULL, UNIQUE(name, password));""")
-	insert = db("""INSERT INTO users(name, password) 
- 				VALUES('admin', 'admin1'), ('secUsr', 'blowfish'), 
-	 			('root', 'root1'), ('Roger', 'Password1'), ('Jonathan', '12345');""")
-	#print("test")
+    create = db(
+        """CREATE TABLE users(id INTEGER PRIMARY KEY,
+ 		name TEXT NOT NULL, password TEXT NOT NULL, UNIQUE(name, password));"""
+    )
+    insert = db(
+        """INSERT INTO users(name, password)
+ 				VALUES('admin', 'admin1'), ('secUsr', 'blowfish'),
+	 			('root', 'root1'), ('Roger', 'Password1'), ('Jonathan', '12345');"""
+    )
+    # print("test")
 
 
 # TODO: Make this run every 10-15 minutes
